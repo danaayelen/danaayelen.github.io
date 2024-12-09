fed0a2e3cf1f16e2657464c3b05f60e7d9301fc7
Dana Ayelen Bazán Domínguez <132296912+danaayelen@users.noreply.github.com>
2024-12-08 23:26:00 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    loop: true
+    loop: true,
+    delay: 60, // Ajusta el tiempo entre cada carácter (en milisegundos)
+    deleteSpeed: 25 // Velocidad al borrar (en milisegundos)
 });
 
 typewriter.typeString('Desarrolladora Frontend Jr.')
-    .pauseFor(2500)
+    .pauseFor(1500) // Reduce la pausa para agilizar el efecto
     .deleteAll()
     .typeString('El único límite de lo que puedes programar es tu imaginación.')
-    .pauseFor(2500)
+    .pauseFor(1500)
     .deleteChars(18)
     .typeString('<strong>, no dudes de ti</strong>')
-    .pauseFor(2500)
-    .start();  
+    .pauseFor(1500)
+    .start();
